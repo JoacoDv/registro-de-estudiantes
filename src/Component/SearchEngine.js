2e11642bbf9f37e6368bf1b75372513432a424b8
JoacoDv <joacodv2003@gmail.com>
2024-10-25 18:48:39 -0300

    HAsta aca llegue

--- src/Component/SearchEngine.js
+++ src/Component/SearchEngine.js
@@ -1,24 +1,39 @@
+import {useState} from "react"
+
 function SearchEngine() {
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [selectOption, setSelectOption] = useState("Materias")
+  const nameValue = e => {
+    setName(e.target.value);
+  }
+  const lastnameValue = e => {
+    setLastname(e.target.value);
+  }
+  const optionsValue = e => {
+    setSelectOption(e.target.value)
+  }
+  function search() {
+    const studentSearch = {nombre: name, apellido: lastname, materia: selectOption};
+    console.log(studentSearch);
+    setName("");
+    setLastname("");
+    setSelectOption("Materia");
+  }
     return (
-        <div className="form-container">
-        <form id="form">
-          <label>
-            <p className="name">Nombre:</p>
-            <input id="name" placeholder="Jhon"></input>
-          </label>
-          <label>
-            <p className="lastname">Apellido:</p>
-            <input id="lastname" placeholder="Doe"></input>
-          </label> <br></br>
-          <select className="select-form-registro" id="select">
-            <option>Materias</option>
-            <option>Matematica</option>
+      <div className="search-engine" >
+        <div>
+          <input placeholder="Jhon" className="name" value={name} onChange={nameValue}></input>
+          <input placeholder="Doe" className="lastname" value={lastname} onChange={lastnameValue}></input>
+          <select className="materia" value={selectOption} onChange={optionsValue}>
+            <option select Value="Materias">Materias</option>
+            <option value="M">Matematica</option>
             <option>Literatura</option>
             <option>Ingles</option>
             <option>Fisica</option>
-          </select> <br></br>
-          <button className="registrar">Buscar</button>
-        </form>
+          </select>
+        </div>
+        <button onClick={search}>Buscar</button>
       </div>
     )
 }
